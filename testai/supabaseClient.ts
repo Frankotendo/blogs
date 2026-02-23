e110292c2a790683cee6faefe1772433a416fcca
Frankotendo <98048267+Frankotendo@users.noreply.github.com>
2026-02-23 03:13:47 -1200

    Add files via upload

---
+++ testai/supabaseClient.ts
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_URL = "https://kzjgihwxiaeqzopeuzhm.supabase.co";
+const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt6amdpaHd4aWFlcXpvcGV1emhtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk2OTU4MDMsImV4cCI6MjA4NTI3MTgwM30.G_6hWSgPstbOi9GgnGprZW9IQVFZSGPQnyC80RROmuw";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
